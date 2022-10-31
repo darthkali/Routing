15497397f9b1d756d60218c9efae6d22c7ec4a37
Danny Steinbrecher <danny.steinbrecher@fh-erfurt.de>
2022-10-31 23:59:08 +0100

    Cleaned up the code

--- Ray-Casting-Algorithm/sketch.js
+++ Ray-Casting-Algorithm/sketch.js
@@ -1,6 +1,6 @@
 const width = 512;
 const height = 512;
-const polygon = [[223, 431], [50, 176], [136, 50], [400, 50], [500, 176], [500, 400], [400, 500], [200, 500]];
+const polygon = [[300, 431], [50, 176], [136, 50], [150, 200], [400, 50], [500, 176], [300, 400], [400, 480], [200, 500], [100, 450]];
 // const polygon = [[225, 431], [50, 176], [136, 19], [225, 158], [314, 19], [400, 176], [225, 431]]
 const redColor = [255, 0, 0];
 const greenColor = [0, 255, 0];
@@ -9,30 +9,21 @@ const pointSize = 5;
 function setup() {
     createCanvas(width, height);
     drawPolygon(polygon);
-noLoop();
+    noLoop();
 }
+
 function draw() {
-    //colorPolygon(polygon);
     noFill();
     stroke(255, 255, 0);
     noStroke();
-    //stroke color
     drawPolygon(polygon);
-
     colorPolygon(polygon);
-    // if(rayCasting([mouseX, mouseY], polygon)){
-    //     fill(greenColor);
-    //     ellipse(mouseX, mouseY, pointSize, pointSize);
-    // }else{
-    //     fill(redColor);
-    //     ellipse(mouseX, mouseY, pointSize, pointSize);
-    // }
 }
 
 function colorPolygon(polygon) {
-    for (let x = 0; x < width/10; x++) {
-        for (let y = 0; y < height/10; y++) {
-            let waypoint = [x*10, y*10];
+    for (let x = 0; x < width / 10; x++) {
+        for (let y = 0; y < height / 10; y++) {
+            let waypoint = [x * 10, y * 10];
             if (rayCasting(waypoint, polygon)) {
                 fill(greenColor);
                 ellipse(waypoint[0], waypoint[1], pointSize, pointSize);
@@ -41,7 +32,6 @@ function colorPolygon(polygon) {
                 ellipse(waypoint[0], waypoint[1], pointSize, pointSize);
             }
         }
-        j = 0
     }
 }
 
@@ -60,7 +50,7 @@ function rayCasting(point, polygon) {
         y = point[1],
         x1, x2, y1, y2;
 
-    for (let i = 0; i < n ; ++i) {
+    for (let i = 0; i < n; ++i) {
         if (i === n - 1) {
             x1 = polygon[i][0];
             x2 = polygon[0][0];
