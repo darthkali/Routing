35009ad7984999c8b3f84deeac2c98d5529b3ad0
Danny Steinbrecher <danny.steinbrecher@fh-erfurt.de>
2022-11-25 09:36:47 +0100

    added function, that calculates the next coordinate with a stepSize

--- backend/src/routing.js
+++ backend/src/routing.js
@@ -9,7 +9,20 @@ const geoJson = require('./geoJsonHandler.js')
 //wenn ja, dann berechne eine neue route
 
 
-
-
 //FH lon 11.039945, 50.984062
 //Bhf lon 11.038489, 50.970533
+// sqrt((fhlon-bhflon)^2 + (fhlat-bhflat)^2) = 1.4km
+// länge zwischen den punkten 0,0136071
+
+// schrittweite = 0,0001
+function calculateAngle(coordinateStart, coordinateEnd) {
+    return Math.atan2(coordinateEnd.lat - coordinateStart.lat, coordinateEnd.lon - coordinateStart.lon);
+}
+
+function calculateNextCoordinate(coordinate, angle, stepSize) {
+    return {
+        lat: coordinate.lat + stepSize * Math.sin(angle),
+        lon: coordinate.lon + stepSize * Math.cos(angle)
+    };
+}
+
